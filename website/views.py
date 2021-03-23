ee17a1bbc92f4f52667a6543aba754db4e354f2b
StefanoS <iiostefanos@gmail.com>
2021-03-23 18:10:06 +0000

    completed contact form reply to user

--- website/views.py
+++ website/views.py
@@ -5,19 +5,23 @@ def home(request):
     return render(request, 'home.html', {})
 
 def contact(request):
-    ''' message_name = request.POST['message-name']
-	    message_email = request.POST['message-email']
-	    message = request.POST['message']
-        # Send email
+    if request.method == "POST":
+        #do stuff
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        #Send email
         send_mail(
-            message_name, # subject
-            message, # message
-            message_email, # from email
-            ['info.dentisto@gmail.com'],  # to email
+            message_name, #subject
+            message, #message
+            message_email, #from email
+            ['info.dentisto@gmail.com'], #to email
             fail_silently=False,
         )
-
         return render(request, 'contact.html', {'message_name': message_name})
     else:
-        #return the page'''
-    return render(request, 'contact.html', {})
+            #return the page
+            return render(request, 'contact.html', {})
+    
+    
