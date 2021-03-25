3f918208dd3538a238c0d173f47f55716fd4c664
StefanoS <iiostefanos@gmail.com>
2021-03-25 15:01:38 +0000

    added and connected base,about, pricing and service.html pages

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
     path('contact.html', views.contact, name="contact"),
+    path('pricing.html', views.pricing, name="pricing"),
+    path('service.html', views.service, name="service"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,6 +4,9 @@ from django.core.mail import send_mail
 def home(request):
     return render(request, 'home.html', {})
 
+def about(request):
+    return render(request, 'about.html', {})   
+
 def contact(request):
     if request.method == "POST":
         #do stuff
@@ -24,4 +27,8 @@ def contact(request):
             #return the page
             return render(request, 'contact.html', {})
     
-    
+def service(request):
+    return render(request, 'service.html', {}) 
+
+def pricing(request):
+    return render(request, 'pricing.html', {})   
